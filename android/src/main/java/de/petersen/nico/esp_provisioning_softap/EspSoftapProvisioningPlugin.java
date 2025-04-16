a2c8fd224ad8fc8a6833d07d516e45948217789f
RamiZain922 <r.zain@nwafthgroup.com>
2025-04-17 01:35:21 +0300

    Update EspSoftapProvisioningPlugin.java

--- android/src/main/java/de/petersen/nico/esp_provisioning_softap/EspSoftapProvisioningPlugin.java
+++ android/src/main/java/de/petersen/nico/esp_provisioning_softap/EspSoftapProvisioningPlugin.java
@@ -11,7 +11,6 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
@@ -44,7 +43,7 @@ public class EspSoftapProvisioningPlugin implements FlutterPlugin, MethodCallHan
   // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
   // depending on the user's project. onAttachedToEngine or registerWith must both be defined
   // in the same class.
-  @SuppressWarnings("deprecation")
+  @SuppressWarnings({"deprecation", "deprecation"})
   public static void registerWith(Registrar registrar) {
     final MethodChannel channel = new MethodChannel(registrar.messenger(), "esp_provisioning_softap");
     channel.setMethodCallHandler(new EspSoftapProvisioningPlugin());
