08ec2caedc1612dcdd4a6f9abcff3bf385801974
RamiZain922 <r.zain@nwafthgroup.com>
2025-04-17 02:12:08 +0300

    Update EspSoftapProvisioningPlugin.java

--- android/src/main/java/de/petersen/nico/esp_provisioning_softap/EspSoftapProvisioningPlugin.java
+++ android/src/main/java/de/petersen/nico/esp_provisioning_softap/EspSoftapProvisioningPlugin.java
@@ -33,21 +33,7 @@ public class EspSoftapProvisioningPlugin implements FlutterPlugin, MethodCallHan
     channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "esp_provisioning_softap");
     channel.setMethodCallHandler(this);
   }
-
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  @SuppressWarnings("deprecation")
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "esp_provisioning_softap");
-    channel.setMethodCallHandler(new EspSoftapProvisioningPlugin());
-  }
+  
 
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
